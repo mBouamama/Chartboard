22d5096ada32a89d5a3396d56ba410fe9796c269
matthieu Bouamama <matthieu.bouamama@leroymerlin.fr>
2020-12-10 19:16:19 +0100

    add default value for config file

--- src/tipboard/app/properties.py
+++ src/tipboard/app/properties.py
@@ -1,26 +1,28 @@
 import json
 import os
 
+from django.core.management.utils import get_random_secret_key
+
 PATH_FOR_PIP = 'src.'  # Location of Tipboard sources
 CONF_DIR = os.path.dirname(os.path.realpath(__file__)) + '/Config/'  # Path of Config directory
 PROPERTIES = json.load(open(CONF_DIR + 'properties.json'))
-TIPBOARD_URL = PROPERTIES['TIPBOARD_URL']
-REDIS_HOST = PROPERTIES['REDIS_HOST']
-REDIS_PORT = PROPERTIES['REDIS_PORT']
-REDIS_PASSWORD = PROPERTIES['REDIS_PASSWORD']
-REDIS_DB = PROPERTIES['REDIS_DB'] if 'REDIS_DB' in PROPERTIES else 0
+TIPBOARD_URL = PROPERTIES['TIPBOARD_URL'] if 'TIPBOARD_URL' in PROPERTIES else "http://0.0.0.0:8080/api"  # default url
+REDIS_HOST = PROPERTIES['REDIS_HOST'] if 'REDIS_HOST' in PROPERTIES else "0.0.0.0"  # default redis host
+REDIS_PORT = PROPERTIES['REDIS_PORT'] if 'REDIS_PORT' in PROPERTIES else 6379  # default redis port
+REDIS_PASSWORD = PROPERTIES['REDIS_PASSWORD'] if 'REDIS_PASSWORD' in PROPERTIES else None  # default redis password
+REDIS_DB = PROPERTIES['REDIS_DB'] if 'REDIS_DB' in PROPERTIES else 0  # default redis database
+API_KEY = PROPERTIES['TIPBOARD_TOKEN'] if 'TIPBOARD_TOKEN' in PROPERTIES else get_random_secret_key() # default token create a random key by default
+DEBUG = PROPERTIES['DEBUG'] if 'DEBUG' in PROPERTIES else True
+LOG = PROPERTIES['LOG'] if 'LOG' in PROPERTIES else True
 BASIC_CONFIG = os.path.join(CONF_DIR, 'default_config.yaml')  # Default layout config
-API_KEY = PROPERTIES['TIPBOARD_TOKEN']
-DEBUG = PROPERTIES['DEBUG']
-LOG = PROPERTIES['LOG']
 BASIC_LAYOUT = 'default_config.yaml'
 BASIC_CONFIG = os.path.join(CONF_DIR, PROPERTIES['default_config'] if 'default_config' in PROPERTIES else BASIC_LAYOUT)
 TIPBOARD_CSS_STYLES = ['css/layout.css']
-TIPBOARD_JAVASCRIPT_FILES = ['js/websocket.js',         # Websocket mecanic
-                             'js/style.js',             # color / responsiv / fliping mecanic
-                             'js/tipboard.js',          # all the client tipboard logic is here
-                             'tiles/chartjs.js',        # simplify the chartJs librari usage
-                             'tiles/text_value.js']     # handle all the other tiles mecanic (stream, txt, etc)
+TIPBOARD_JAVASCRIPT_FILES = ['js/websocket.js',  # Websocket mecanic
+                             'js/style.js',  # color / responsiv / fliping mecanic
+                             'js/tipboard.js',  # all the client tipboard logic is here
+                             'tiles/chartjs.js',  # simplify the chartJs librari usage
+                             'tiles/text_value.js']  # handle all the other tiles mecanic (stream, txt, etc)
 FLIPBOARD_INTERVAL = PROPERTIES['FLIPBOARD_INTERVAL'] if 'FLIPBOARD_INTERVAL' in PROPERTIES else 10
 
 ALLOWED_TILES = ['text', 'simple_percentage', 'listing', 'big_value', 'just_value',  # Homemade
@@ -31,20 +33,20 @@ ALLOWED_TILES = ['text', 'simple_percentage', 'listing', 'big_value', 'just_valu
                  'iframe', 'stream', 'custom']  # misc
 
 COLOR_TAB = [  # material color
-    'rgba(66, 165, 245, 0.8)',      # blue #42a5f5
-    'rgba(114, 191, 68, 0.8)',      # green #72bf44
-    'rgba(0, 150, 136, 0.8)',       # teal #009688
-    'rgba(255, 234, 0, 0.8)',       # yellow #ffea00
-    'rgba(255, 152, 0, 0.8)',       # orange #ff9800
-    'rgba(213, 0, 0, 0.8)',         # red #d50000
-    'rgb(240, 98, 146)',            # pink #f06292
-    'rgba(224, 224, 224, 0.8)',     # Grey #e0e0e0
-    'rgb(177, 208, 225)',           # Light Blue #b1d0e1
-    'rgba(121, 85, 72, 0.8)',       # Marron #795548
-    'rgba(149, 117, 205, 0.8)',     # purple #9575cd
-    'rgb(224, 64, 251)',            # Light Purple #e040fb
-    'rgb(48, 79, 254)',             # Indigo #304ffe
-    'rgb(33, 33, 33)'               # Black #212121
+    'rgba(66, 165, 245, 0.8)',  # blue #42a5f5
+    'rgba(114, 191, 68, 0.8)',  # green #72bf44
+    'rgba(0, 150, 136, 0.8)',  # teal #009688
+    'rgba(255, 234, 0, 0.8)',  # yellow #ffea00
+    'rgba(255, 152, 0, 0.8)',  # orange #ff9800
+    'rgba(213, 0, 0, 0.8)',  # red #d50000
+    'rgb(240, 98, 146)',  # pink #f06292
+    'rgba(224, 224, 224, 0.8)',  # Grey #e0e0e0
+    'rgb(177, 208, 225)',  # Light Blue #b1d0e1
+    'rgba(121, 85, 72, 0.8)',  # Marron #795548
+    'rgba(149, 117, 205, 0.8)',  # purple #9575cd
+    'rgb(224, 64, 251)',  # Light Purple #e040fb
+    'rgb(48, 79, 254)',  # Indigo #304ffe
+    'rgb(33, 33, 33)'  # Black #212121
 ]
 BACKGROUND_TAB = ['#4caf50', '#ff6d00', '#d50000']
 print(f"[DEBUG] Tipboard start in DEBUG MODE:{DEBUG} & LOG:{LOG}", flush=True)
